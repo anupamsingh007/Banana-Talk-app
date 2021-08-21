84209d03d47d3705257ac1255b53be3fb3c4f914
anupamsingh007 <75076385+anupamsingh007@users.noreply.github.com>
2021-08-21 01:27:48 -0700

    updated

---
+++ index.js
@@ -0,0 +1,31 @@
+var translateInput = document.querySelector("#input");
+var translateButton = document.querySelector("#btn");
+var translateOutput = document.querySelector("output");
+
+var url = "https://api.funtranslations.com/translate/minion.json"
+
+function translationUrl(input) {
+    return url + "?" + "text=" + input
+}
+
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("something is wrong. Try again later.")
+}
+
+function clickHandler() {
+    var inputText = translateInput.value;
+
+
+    fetch(translationUrl(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+
+            translateOutput.innerText = translatedText;
+        })
+        .catch(errorHandler)
+};
+
+
+translateButton.addEventListener("click", clickHandler)
